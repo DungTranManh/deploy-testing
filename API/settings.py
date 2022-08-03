35341c8b9457270fb944debe06c6151ea6946e20
DungTranManh <manhdung5801@gmail.com>
2022-08-03 20:35:54 +0700

    no message

--- API/settings.py
+++ API/settings.py
@@ -121,7 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 REST_FRAMEWORK = {
     'DEFAULT_THROTTLE_CLASSES': (
         'rest_framework.throttling.AnonRateThrottle',
