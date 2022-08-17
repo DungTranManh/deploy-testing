5b84d100435c499f81ead989e2879072c8515f32
DungTranManh <manhdung5801@gmail.com>
2022-08-17 21:22:01 +0700

    update

--- search/admin.py
+++ search/admin.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from .models import Data
 # Register your models here.
 
-@admin.register(Data)
+# @admin.register(Data)
 class DataAdmin(admin.ModelAdmin):
     list_display = ('ent_seq','keb','reb','trans_det')
     # list_display = ('keb','ent_seq','reb','trans_det')
     list_editable = ('keb','reb','trans_det')
+    list_per_page = 8
+admin.site.register(Data,DataAdmin)
