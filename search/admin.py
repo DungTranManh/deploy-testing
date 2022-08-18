2ccfe93146de954e315cac8aa774e8353f82e50f
DungTranManh <manhdung5801@gmail.com>
2022-08-18 10:46:30 +0700

    update ui admin site (dimension of text fields)

--- search/admin.py
+++ search/admin.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
 from .models import Data
+from django.db import models
+from django.forms import Textarea
 # Register your models here.
 
 # @admin.register(Data)
 class DataAdmin(admin.ModelAdmin):
+    formfield_overrides = { models.TextField: {'widget': Textarea(
+                       attrs={'rows': 3,
+                              'cols': 30})},
+    }
     list_display = ('ent_seq','keb','reb','trans_det','tieng_viet')
     # list_display = ('keb','ent_seq','reb','trans_det')
     list_editable = ('keb','reb','trans_det','tieng_viet')
-    list_per_page = 8
+    list_per_page = 9
 admin.site.register(Data,DataAdmin)
