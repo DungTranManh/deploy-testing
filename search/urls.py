5d14e0e01e4381a4562853ccffa8b05a2d99b7bc
DungTranManh <manhdung5801@gmail.com>
2022-08-19 11:15:15 +0700

    update views and search page frontend

--- search/urls.py
+++ search/urls.py
@@ -1,6 +1,8 @@
 from django.urls import include, path
 from.views import  SearchAPIView_keb, SearchAPIView_reb
+from . import views
 urlpatterns = [
     path('search/keb/<str:keb_text>/', SearchAPIView_keb.as_view(), name='SearchAPIkeb'),
-    path('search/reb/<str:reb_text>/', SearchAPIView_reb.as_view(), name= 'SearchAPIreb' )
+    path('search/reb/<str:reb_text>/', SearchAPIView_reb.as_view(), name= 'SearchAPIreb'),
+    path('search/',views.index, name='index')
 ]
--- search/views.py
+++ search/views.py
@@ -1,3 +1,5 @@
+from asyncio.windows_events import NULL
+from tkinter.tix import CheckList
 from django.shortcuts import render
 from rest_framework import filters
 from rest_framework.views import APIView
@@ -5,8 +7,29 @@ from rest_framework.response import Response
 from rest_framework import status
 from .serializers import SearchSerializer
 from .models import Data
+from django.db.models import Q
 # Create your views here.
 
+def index(request):
+    datas_search = request.GET.get('searchbar_value')
+    checked_box = request.GET.get('flexRadioDefault')
+    data = Data.objects.all()
+    if checked_box == 'keb&reb':
+        if datas_search:
+            data = Data.objects.filter(Q(keb = datas_search) | Q(reb=datas_search))
+        else:
+            data = Data.objects.all()
+    elif checked_box == 'keb':
+        if datas_search:
+            data = Data.objects.filter(Q(keb = datas_search))
+        else:
+            data = Data.objects.all()
+    elif checked_box == 'reb':
+        if datas_search:
+            data = Data.objects.filter(Q(reb=datas_search))
+        else:
+            data = Data.objects.all()
+    return render(request, 'search/index.html',{'datas':data})
 
 class SearchAPIView_keb(APIView):
     def get(self,request,keb_text):
@@ -26,4 +49,4 @@ class SearchAPIView_reb(APIView):
             data_get_serializer = SearchSerializer(data_get, many = True)
             return Response(data = data_get_serializer.data,status=status.HTTP_200_OK)
 
-        
+
